be6b03806248f72b0a450f45758c222a88da983a
Quincy <quincy@qedlaboratory.com>
2025-03-18 13:32:26 -0400

    GoL code

---
+++ src/simulations/gol.h
@@ -0,0 +1,15 @@
+#ifndef GOL_H
+#define GOL_H
+
+
+
+void sim_gol_init(void);
+void sim_gol_destroy(void);
+void sim_gol_update(float dt);
+void sim_gol_params_ui(void);
+void sim_gol_plot_ui(void);
+void sim_gol_render(void);
+
+
+
+#endif /* GOL_H */
